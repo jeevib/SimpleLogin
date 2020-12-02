ee07e0ca0ffa227c1ed38067761a1e6bab5a249b
jeevib <jeevi.sujee.23@gmail.com>
2020-12-02 14:01:24 +0530

    access modifier tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
 
 	validates_presence_of :email
     validates_uniqueness_of :email
+
+    def say_secret_with_self
+	    self.secret
+	    # secret
+	end
+
+	protected
+
+	def secret
+		"secret"
+	end
 end
